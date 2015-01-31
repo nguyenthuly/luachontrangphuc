2ded990c2bdfc5c1a1d7c97d58ea867d931dbdd2
Nguyễn Thu Lý <lynt@nitrotech.vn>
2015-01-31 10:10:49 +0700

    add item into scroll view

--- SwiftArchitecture/Classes/MainVC/DressTime/SWDressTimeViewController.m
+++ SwiftArchitecture/Classes/MainVC/DressTime/SWDressTimeViewController.m
@@ -9,7 +9,7 @@
 #import "SWDressTimeViewController.h"
 #import "SWWeatherView.h"
 
-@interface SWDressTimeViewController ()
+@interface SWDressTimeViewController () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *cityLabel;
 @property (weak, nonatomic) IBOutlet UILabel *dateTimeLabel;
 @property (weak, nonatomic) IBOutlet UILabel *currentTimeLabel;
@@ -20,7 +20,6 @@
 @property (weak, nonatomic) IBOutlet UIButton *jeanButton;
 @property (weak, nonatomic) IBOutlet UIButton *skirtButton;
 @property (weak, nonatomic) IBOutlet UIButton *shoeButton;
-
 @property (strong, nonatomic) IBOutlet UIScrollView *weatherScrollView;
 
 - (IBAction)skirtButton:(id)sender;
@@ -32,29 +31,61 @@
 
 @end
 
-@implementation SWDressTimeViewController
+@implementation SWDressTimeViewController {
+    NSMutableArray *weatherArr;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     [self initUI];
     [self initData];
+    [self initScroll];
 }
 
 - (void)initUI{
     self.title = DressTime_Title;
-    self.weatherScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 231, 400,128)];
-    SWWeatherView *weatherView = [[SWWeatherView alloc] init];
-    [self.weatherScrollView addSubview:weatherView];
-    [weatherView setBackgroundColor:[UIColor greenColor]];
-    [self.weatherScrollView setBackgroundColor:[UIColor redColor]];
+//    self.weatherScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 231, 400,128)];
+//    SWWeatherView *weatherView = [[SWWeatherView alloc] init];
+//    [self.weatherScrollView addSubview:weatherView];
+//    [weatherView setBackgroundColor:[UIColor greenColor]];
+//    [self.weatherScrollView setBackgroundColor:[UIColor redColor]];
     //[self.view addSubview:self.weatherScrollView];
+    self.weatherScrollView.delegate = self;
+    self.weatherScrollView.hidden = NO;
     
     self.chooseView.hidden = YES;
     self.chooseView.alpha = 0;
 }
 
 - (void)initData{
+    weatherArr = [[NSMutableArray alloc] initWithArray:@[@22,@23,@32,@21,@12,@13,@15,@16,@17]];
+}
+
+- (void)initScroll {
+    int xPos = 0;
+    
+    for (int i=0; i < [weatherArr count]; i++) {
+        SWWeatherView *weatherView = [[SWWeatherView alloc] initWithFrame:CGRectZero];
+        weatherView.backgroundColor = [UIColor blueColor];
+        NSString *temperature = [NSString stringWithFormat:@"%@ ºC", [weatherArr objectAtIndex:i]];
+        weatherView.temperatureLabel.text = temperature;
+        CGRect frame = [weatherView frame];
+        frame.origin.x = xPos;
+        xPos += frame.size.width;
+        [weatherView setFrame:frame];
+        [self.weatherScrollView addSubview:weatherView];
+        
+        NSLog(@"F:%@ - %d", NSStringFromCGRect(weatherView.frame), xPos);
+    }
+    
+    CGSize contentSize = [self.weatherScrollView contentSize];
+    contentSize.width = xPos;
+    [self.weatherScrollView setContentSize:contentSize];
+    NSLog(@"F:%@", NSStringFromCGRect(self.weatherScrollView.frame));
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
     
 }
 
--- SwiftArchitecture/Views/SWWeatherView.m
+++ SwiftArchitecture/Views/SWWeatherView.m
@@ -10,12 +10,26 @@
 
 @implementation SWWeatherView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (id)initWithFrame:(CGRect)frame
+{
+    NSArray *array = [[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:self options:nil];
+    if (array == nil || [array count] == 0)
+        return nil;
+    frame = [[array objectAtIndex:0] frame];
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        [self addSubview:[array objectAtIndex:0]];
+        
+        UITapGestureRecognizer *tapGestureViewController = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapOptionGrid)];
+        [self addGestureRecognizer:tapGestureViewController];
+    }
+    return self;
 }
-*/
 
+- (void)didTapOptionGrid
+{
+
+}
 @end
